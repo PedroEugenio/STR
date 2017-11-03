0ea765424233ca5a8250329d022373b13e2f1e1d
Pedro Eugénio <p.geno_88@hotmail.com>
2017-11-03 15:18:35 +0000

    Code organized from ex3, but ex4 still not working

--- Labwork#1/sources/ex4.c
+++ Labwork#1/sources/ex4.c
@@ -15,22 +15,29 @@
 #include "../resources/timespec.h"
 
 
-/* Activation periods */
+// Activation periods
 #define PERIOD1_MS 100
 #define PERIOD2_MS 200
 #define PERIOD3_MS 300 
 
-/* Activation time for each thread */
+#define NUMBER_THREADS 3
+
+// Activation time for each thread 
 struct timespec activation_time[3];
 
 /*
-  Variable to hold the worst response time
-*/
-struct timespec worse_response_time[3] = { {0,0}, {0,0}, {0,0}};
+  switch_time - time when to switch priority orders
+  switch_dt - the time between priority switches
+ */
+struct timespec switch_time, switch_dt;
 
-/*
-  This thread will run f1()
-*/
+
+// Variable to hold the worst response time
+struct timespec worse_response_time[6] = { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}};
+
+pthread_t thr[NUMBER_THREADS];
+
+//  This thread will run f1() and switch the priority order from RMPO to it's inverse
 void* function1(void* arg){
   //wait so that all tasks activate at the same time
   clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &activation_time[0], NULL);
@@ -139,90 +146,94 @@ void* function3(void* arg){
 
 int main(){
 
+    cpu_set_t my_set;
+    pthread_attr_t attr[NUMBER_THREADS];
+    struct sched_param priority[3];
+    // Set all activation to 2 seconds after the current time
+    struct timespec dt; dt.tv_sec = 2; dt.tv_nsec = 0;
 
-  /* Set all activation to 2 seconds after the current time*/
-  struct timespec dt; dt.tv_sec = 2; dt.tv_nsec = 0;
-  clock_gettime(CLOCK_MONOTONIC, &activation_time[0]);
-  for(int i = 1; i < 3; i++){
-    activation_time[i] = activation_time[0];
-    activation_time[i].tv_sec += dt.tv_sec;
-  }
-  activation_time[0].tv_sec += dt.tv_sec;
-
-  //lock memory
-  mlockall(MCL_CURRENT|MCL_FUTURE);
-
-  pthread_t thr[3];
-
-  /*
-    For each thread change the scheduler to FIFO and set the priorities (in RMPO)
-  */
-  pthread_attr_t attr[3];
-  pthread_attr_init(&attr[0]);
-  pthread_attr_setschedpolicy(&attr[0], SCHED_FIFO);
-  struct sched_param priority[3]; priority[0].sched_priority = sched_get_priority_max(SCHED_FIFO);
-  pthread_attr_setschedparam(&attr[0],&priority[0]);
-  pthread_attr_setinheritsched(&attr[0], PTHREAD_EXPLICIT_SCHED);
+    //lock memory 
+    mlockall(MCL_CURRENT|MCL_FUTURE);
+  
+    // Set threads affinity to CPU 0
+    CPU_ZERO(&my_set);
+    CPU_SET(0, &my_set);
+  
+    clock_gettime(CLOCK_MONOTONIC, &activation_time[0]);
+    for(int i = 1; i < 3; i++){
+        activation_time[i] = activation_time[0];
+        activation_time[i].tv_sec += dt.tv_sec;
+    }
+    activation_time[0].tv_sec += dt.tv_sec;
 
 
-  pthread_attr_init(&attr[1]);
-  pthread_attr_setschedpolicy(&attr[1], SCHED_FIFO);
-  priority[1].sched_priority = sched_get_priority_max(SCHED_FIFO)-1;
-  pthread_attr_setschedparam(&attr[1],&priority[1]);
-  pthread_attr_setinheritsched(&attr[1], PTHREAD_EXPLICIT_SCHED);
+    // For each thread change the scheduler to FIFO and set the priorities (in RMPO)
+    
+    // Set the parameters of threads
+    pthread_attr_init(&attr[0]);
+    pthread_attr_init(&attr[1]);
+    pthread_attr_init(&attr[2]);
 
-  pthread_attr_init(&attr[2]);
-  pthread_attr_setschedpolicy(&attr[2], SCHED_FIFO);
-  priority[2].sched_priority = sched_get_priority_max(SCHED_FIFO)-2;
-  pthread_attr_setschedparam(&attr[2],&priority[2]);
-  pthread_attr_setinheritsched(&attr[2], PTHREAD_EXPLICIT_SCHED);
+    // Set affinity of all threads, preventing biased times
+    pthread_attr_setaffinity_np(&attr[0], sizeof(my_set), &my_set);
+    pthread_attr_setaffinity_np(&attr[1], sizeof(my_set), &my_set);
+    pthread_attr_setaffinity_np(&attr[2], sizeof(my_set), &my_set);
 
-  /*
-    Set threads affinity to CPU 0
-  */
-  cpu_set_t my_set;
-  CPU_ZERO(&my_set);
-  CPU_SET(0, &my_set);
-  pthread_attr_setaffinity_np(&attr[0], sizeof(my_set), &my_set);
-  pthread_attr_setaffinity_np(&attr[1], sizeof(my_set), &my_set);
-  pthread_attr_setaffinity_np(&attr[2], sizeof(my_set), &my_set);
+    // Set Explicit scheduling
+    pthread_attr_setinheritsched(&attr[0], PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setinheritsched(&attr[1], PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setinheritsched(&attr[2], PTHREAD_EXPLICIT_SCHED);
+
+    // Set Policy type to SCHED_FIFO
+    pthread_attr_setschedpolicy(&attr[0], SCHED_FIFO);
+    pthread_attr_setschedpolicy(&attr[1], SCHED_FIFO);
+    pthread_attr_setschedpolicy(&attr[2], SCHED_FIFO);
+
+    // Defines the priority of each thread
+    priority[0].sched_priority = sched_get_priority_max(SCHED_FIFO);
+    priority[1].sched_priority = sched_get_priority_max(SCHED_FIFO)-1;
+    priority[2].sched_priority = sched_get_priority_max(SCHED_FIFO)-2;
+
+    /* Sets the scheduling parameter attributes of the thread attributes object attr
+    to the values specified in the buffer pointed to by priority */
+    pthread_attr_setschedparam(&attr[0],&priority[0]);
+    pthread_attr_setschedparam(&attr[1],&priority[1]);
+    pthread_attr_setschedparam(&attr[2],&priority[2]);
+
+    // Create all the threads
+    printf("Starting threads, please wait\n");
+    if(pthread_create(&(thr[0]), &attr[0], function1, NULL) < 0){
+        printf("Error %s\n",strerror(errno));
+        exit(0);
+    }
+    if(pthread_create(&(thr[1]), &attr[1], function2, NULL) < 0){
+        printf("Error %s\n",strerror(errno));
+        exit(0);
+    }
+    if(pthread_create(&(thr[2]), &attr[2], function3, NULL) < 0){
+        printf("Error %s\n",strerror(errno));
+        exit(0);
+    }
 
 
-  /* Create all the threads */
-  printf("Starting threads, please wait\n");
+    /*
+        This will sleep for 5 seconds, then cancel all threads and print the worst
+        response times.
+    */
+    while(1){
+        sleep(5);
+        pthread_cancel(thr[0]);
+        pthread_cancel(thr[1]);
+        pthread_cancel(thr[2]);
+        printf("1 - worse response time: %LF ms\n", time2ms(worse_response_time[0]));
+        worse_response_time[0].tv_sec = 0; worse_response_time[0].tv_nsec = 0;
+        printf("2 - worse response time: %LF ms\n", time2ms(worse_response_time[1]));
+        worse_response_time[1].tv_sec = 0; worse_response_time[1].tv_nsec = 0;
+        printf("3 - worse response time: %LF ms\n", time2ms(worse_response_time[2]));
+        worse_response_time[2].tv_sec = 0; worse_response_time[2].tv_nsec = 0;
 
-  if(pthread_create(&(thr[0]), &attr[0], function1, NULL) < 0){
-    printf("Error %s\n",strerror(errno));
-    exit(0);
-  }
-  if(pthread_create(&(thr[1]), &attr[1], function2, NULL) < 0){
-    printf("Error %s\n",strerror(errno));
-    exit(0);
-  }
-  if(pthread_create(&(thr[2]), &attr[2], function3, NULL) < 0){
-    printf("Error %s\n",strerror(errno));
-    exit(0);
-  }
-
-
-  /*
-      This will sleep for 5 seconds, then cancel all threads and print the worst
-    response times.
-  */
-  while(1){
-    sleep(5);
-    pthread_cancel(thr[0]);
-    pthread_cancel(thr[1]);
-    pthread_cancel(thr[2]);
-    printf("1 - worse response time: %LF ms\n", time2ms(worse_response_time[0]));
-    worse_response_time[0].tv_sec = 0; worse_response_time[0].tv_nsec = 0;
-    printf("2 - worse response time: %LF ms\n", time2ms(worse_response_time[1]));
-    worse_response_time[1].tv_sec = 0; worse_response_time[1].tv_nsec = 0;
-    printf("3 - worse response time: %LF ms\n", time2ms(worse_response_time[2]));
-    worse_response_time[2].tv_sec = 0; worse_response_time[2].tv_nsec = 0;
-
-    exit(0);
-  }
+        exit(0);
+    }
 
 
 }
