7a51fc2880d8ef9c48402294fab3d554e3e523e0
Frederico Vaz <fredvaz8@gmail.com>
2017-11-16 10:24:23 +0000

    Ex1 begin!

--- Labwork#2/sources/Ex1.c
+++ Labwork#2/sources/Ex1.c
@@ -12,19 +12,45 @@ int main(){
     FILE *fptr;
     fptr = fopen("point_cloud.txt","r");  // Open the file
     if(fptr == NULL){
-        perror("fopen()"); 
-        exit(1);             
+        perror("fopen()");
+        exit(1);
     }
-        
+
     // Verify if the document reached to an end
-    while( !feof (fptr) ){ 
+    while( !feof (fptr) ){
         // Saves the values from .dat file to the variables
         fscanf(fptr, "%f %f %f %f", &coord[i].x, &line_body[i].y, &line_body[i].z, &line_body[i].m);
         //printf("%.1f %.1f %.1f %.1f \n", line_body[i].x, line_body[i].y, line_body[i].z, line_body[i].m);
         i++;
     }
-        
+
     fclose(fptr);
     printf("Closing the file \n");
     return 0;
+}
+
+void calc_min() {
+
+
+
+}
+
+void calc_max() {
+
+
+}
+
+void calc_average() {
+
+  for(int i = 0; i < NUM_POINTS; i++){
+    sum += coord[i];
+  }
+
+
+}
+
+void  calc_std() {
+
+
+
 }
