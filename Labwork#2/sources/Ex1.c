8e8d95fd84cd1251c86d9e7d98d2a794124ed311
Frederico Vaz <fredvaz8@gmail.com>
2017-11-16 10:56:51 +0000

    print min, max, average and std

--- Labwork#2/sources/Ex1.c
+++ Labwork#2/sources/Ex1.c
@@ -11,7 +11,7 @@ struct Coord{
 
 int main(){
     struct Coord coord[NUM_POINTS];
-    printf("Opening the file \n");
+    printf("Opening the file...\n");
     FILE *fptr;
     fptr = fopen("../resources/point_cloud1.txt","r");  // Open the file
     if(fptr == NULL){
@@ -19,18 +19,34 @@ int main(){
         exit(1);
     }
 
-    
+
     int i=0;
     // Verify if the document reached to an end
-    while( !feof (fptr) ){ 
+    while( !feof (fptr) ){
         // Saves the values from .txt file to the variables
         fscanf(fptr, "%f %f %f", &coord[i].x, &coord[i].y, &coord[i].z);
         printf("%.4f %.4f %.4f \n", coord[i].x, coord[i].y, coord[i].z);
         i++;
     }
-  
     fclose(fptr);
-    printf("Closing the file \n");
+    printf("Closing the file...\n");
+
+
+    /* Calculate min, max, average and std*/
+    printf("We are calculate...\n", );
+
+    /* a point to float array */
+    int float average = calc_average(coord);
+
+    printf("Average - x: %lf y: %lf z: %lf\n", *(average), *(average+1), *(average+2));
+
+    //printf("Min - x: %lf y: %lf z: %lf\n", );
+    //printf("Max - x: %lf y: %lf z: %lf\n", );
+    printf("Average - x: %lf y: %lf z: %lf\n", );
+    //printf("STD - x: %lf y: %lf z: %lf\n", );
+
+
+
     return 0;
 }
 /*******************************************************************************
@@ -68,6 +84,10 @@ float * calc_average() {
     average[1] += coord[i].y;
     average[2] += coord[i].z;
   }
+  average[0] = average[0]/NUM_POINTS;
+  average[1] = average[0]/NUM_POINTS;
+  average[2] = average[0]/NUM_POINTS;
+
   return average;
 }
 /*******************************************************************************
