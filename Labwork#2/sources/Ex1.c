363a0573939c6475b715e66188187c176704a9c6
Frederico Vaz <fredvaz8@gmail.com>
2017-12-11 20:13:04 +0000

    Threads implementation

--- Labwork#2/sources/Ex1.c
+++ Labwork#2/sources/Ex1.c
@@ -10,19 +10,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
+#include <errno.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sched.h>
 
-#define NUM_POINTS 20000
-#define NUM_FILES  3
+/* MAX number of Points that our program can read */
+#define NUM_POINTS   20000
+/* MAX number of files that our programa can read */
+#define NUM_FILES    3
 
+#define POINTCLOUD1  1
+#define POINTCLOUD2  1
+#define POINTCLOUD3  3
+
+/* Scheduler RMPO */
+#define POLICY2USE SCHED_FIFO //SCHED_RR // SCHED_FIFO
+
+/* Finish Time of threads in s */
+#define FINISH       10
+
+/* Struct to store coordinates from Point Clouds files */
 struct Coord{
     float x;
     float y;
     float z;
 };
 
-int count_points = 0; //Global variable acessible in all functions
+/* Counter to number of points readed in original files */
+int count_points = 0;
+/* Counter to number of points writed in filtered files */
 int count_filtered_points = 0;
 
+/* One threads for each file */
+pthread_t thread[NUM_FILES];
+
+/* Activation time for each thread */
+struct timespec response_time[NUM_FILES] = {{0,0}, {0,0}, {0,0}};
+
+/* Error Message */
+#ifndef errorExit
+#define errorExit(msg) do{ perror(msg); exit(EXIT_FAILURE); }while(0)
+#endif
+
+/* Global declaration */
+void* pointCloud1(void *arg);
+void* pointCloud2(void *arg);
+void* pointCloud3(void *arg);
+void execute_math(int number_points);
+void response_times();
 void read_file(struct Coord *coord, FILE *fptr, int file);
 void write_file(struct Coord *coord, FILE *fptr, int file);
 void calc_average(struct Coord *coord,float *average, int number_points);
@@ -34,93 +71,173 @@ void denoise(struct Coord *coord, float *std);
 
 
 int main(){
+    printf("\n\nPart I - Threads & Semaphores\n\n");
 
     struct Coord coord[NUM_POINTS]; // original file
     struct Coord filter[NUM_POINTS]; // filtered file
 
-    FILE *readfile;
-    FILE *writefile;
-    float average[3];
-    float min[3];
-    float max[3];
-    float std[3]; //standard deviation
+    struct timespec current_time;
 
-    // Read, Calculate and Write for each File:
+    /* Lock all of the calling process's virtual address space into RAM  Note: requires "sudo" to lock the memory. */
+    if( mlockall(MCL_CURRENT | MCL_FUTURE) == -1 )
+      errorExit("main->mloackall");
+
+    cpu_set_t mask;
+    pthread_attr_t attr[NUM_FILES];
+    struct sched_param priority[NUM_FILES];
+
+    /* Set all threads affinity to CPU0 */
+    CPU_ZERO(&mask);
+    CPU_SET(0, &mask);
+
+
+    /* One thread for each file: */
     for (int NUMBER_FILE = 1; NUMBER_FILE <= NUM_FILES; NUMBER_FILE++) {
 
-      readfile = (FILE *)malloc(sizeof(FILE));
-      read_file(coord, readfile, NUMBER_FILE);
-      free(readfile);
+      /* Parameters of threads */
+      if( pthread_attr_init(&(attr[i])) != 0 )
+        errorExit("main->pthread_attr_init");
 
-      /* --------------------------------------------------------------------------------------------- */
-      printf("========================================================\n");
+      /* Sets the affinity of all threads to the previously defined core */
+      if( pthread_attr_setaffinity_np(&(attr[i]), sizeof(cpu_set_t), &mask) != 0 )
+        errorExit("main->pthread_attr_setaffinity");
 
-      printf("Number of points: %i\n", count_points);
+      /* By predefinition, the threads executes with implicit scheduling */
+      if( pthread_attr_setinheritsched(&(attr[i]), PTHREAD_EXPLICIT_SCHED) != 0 )
+        errorExit("main->pthread_attr_setinheritsched");
 
-      calc_average(coord, average, count_points);
-      printf("Average Value :: x:%.4f y:%.4f z:%.4f\n", average[0], average[1], average[2]);
-
-      calc_min(coord, min, count_points);
-      printf("Minimum Value :: x:%.4f y:%.4f z:%.4f \n", min[0], min[1], min[2]);
-
-      calc_max(coord, max, count_points);
-      printf("Maximum Value :: x:%.4f y:%.4f z:%.4f \n", max[0], max[1], max[2]);
-
-      calc_std(coord, average, std, count_points);
-      printf("Standard Deviation Value :: x:%.4f y:%.4f z:%.4f \n", std[0], std[1], std[2]);
-
-      printf("========================================================\n");
-      /* --------------------------------------------------------------------------------------------- */
-
-      x_negative_filter(coord, filter);
-
-      printf("After filtering: number of points = %i\n", count_filtered_points);
-
-      calc_average(filter, average, count_filtered_points);
-      printf("Average Value :: x:%.4f y:%.4f z:%.4f\n", average[0], average[1], average[2]);
-
-      calc_min(filter, min, count_filtered_points);
-      printf("Minimum Value :: x:%.4f y:%.4f z:%.4f \n", min[0], min[1], min[2]);
-
-      calc_max(filter, max, count_filtered_points);
-      printf("Maximum Value :: x:%.4f y:%.4f z:%.4f \n", max[0], max[1], max[2]);
-
-      calc_std(filter, average, std, count_filtered_points);
-      printf("Standard Deviation Value :: x:%.4f y:%.4f z:%.4f \n", std[0], std[1], std[2]);
+      /* Scheduler */
+      if( pthread_attr_setschedpolicy(&(attr[i]), POLICY2USE) != 0 )
+        errorExit("main->pthread_attr_setschedpolicy");
 
 
-      printf("========================================================\n");
-      /* --------------------------------------------------------------------------------------------- */
+      /* All threads with the same priority value */
+      memset(&(priority[i]), 0, sizeof(struct sched_param));
+      if( (priority[i].sched_priority = sched_get_priority_max(POLICY2USE)) == -1)
+        errorExit("main->sched_get_priority_max");
 
-      denoise(filter, std);
+      printf("Priority of Task %d: %d\n", i+1, priority[i].sched_priority);
 
-      printf("STD filtering: number of points = %i\n", count_filtered_points);
-      calc_average(filter, average, count_filtered_points);
-      printf("Average Value :: x:%.4f y:%.4f z:%.4f\n", average[0], average[1], average[2]);
+      if( pthread_attr_setschedparam(&(attr[i]), &(priority[i])) != 0 )
+      errorExit("main->pthread_attr_setschedparam");
 
-      calc_min(filter, min, count_filtered_points);
-      printf("Minimum Value :: x:%.4f y:%.4f z:%.4f \n", min[0], min[1], min[2]);
-
-      calc_max(filter, max, count_filtered_points);
-      printf("Maximum Value :: x:%.4f y:%.4f z:%.4f \n", max[0], max[1], max[2]);
-
-      calc_std(filter, average, std, count_filtered_points);
-      printf("Standard Deviation Value :: x:%.4f y:%.4f z:%.4f \n", std[0], std[1], std[2]);
-
-      printf("========================================================\n");
-      /* --------------------------------------------------------------------------------------------- */
-
-      writefile = (FILE *)malloc(sizeof(FILE));
-      write_file(filter, writefile, NUMBER_FILE);
-      free(writefile);
-
-      // Counters points Reset
-      count_points = 0;
-      count_filtered_points = 0;
     }
-    return 0;
-}
 
+    /* Create threads for all Point Cloud - Executation of the Tasks */
+    if(pthread_create(&(thread[0]), &(attr[0]), pointCloud1, NULL) < 0)
+      errorExit("main->pthread_create");
+
+    if(pthread_create(&(thread[1]), &(attr[1]), pointCloud2, NULL) < 0)
+      errorExit("main->pthread_create");
+
+    if(pthread_create(&(thread[2]), &(attr[2]), pointCloud3, NULL) < 0)
+      errorExit("main->pthread_create");
+
+
+    /* Final results... for each Point Cloud */
+    do {
+      printf("\n\nWaiting...\n\n");
+      /* Main thread go to sleep when wake up it will cancel tasks threads */
+      sleep(FINISH);
+
+      for (int i = 0; i < NUM_FILES; i++) {
+        pthread_cancel(thread[i]);
+      }
+      printf("Inverse RMPO response times:\n");
+      response_times();
+
+      exit(EXIT_SUCCESS);
+    } while(1);
+
+    /* For each file to read, calculate and write: */
+    for (int NUMBER_FILE = 1; NUMBER_FILE <= NUM_FILES; NUMBER_FILE++) {
+
+    }
+}
+void* pointCloud1(void *arg) {
+
+  FILE *readfile;
+  FILE *writefile;
+
+  /* Read orginal file */
+  readfile = (FILE *)malloc(sizeof(FILE));
+  read_file(coord, readfile, POINTCLOUD1);
+  free(readfile);
+
+  execute_math(count_points);
+
+  /* Removing axis X negavtive Points */
+  x_negative_filter(coord, filter);
+  printf("After filtering...\n\n");
+  execute_math(count_filtered_points);
+
+  /* STD filtering - Removing Noising Points */
+  denoise(filter, std);
+  printf("After filtering...\n\n");
+  execute_math(count_filtered_points);
+
+  /* Store in other file */
+  writefile = (FILE *)malloc(sizeof(FILE));
+  write_file(filter, writefile, NUMBER_FILE);
+  free(writefile);
+
+  /* Counters points Reset */
+  count_points = 0;
+  count_filtered_points = 0;
+}
+void* pointCloud2(void *arg) {
+
+}
+void* pointCloud3(void *arg) {
+
+}
+/*******************************************************************************
+*
+* Objective: Math tasks to do in each Point Cloud
+* Issues:
+*
+*******************************************************************************/
+void execute_math(int number_points) {
+
+  float average[3];
+  float min[3];
+  float max[3];
+  float std[3]; //standard deviation
+
+  printf("========================================================\n");
+
+  printf("Number of points: %i\n", number_points);
+
+  calc_average(coord, average, number_points);
+  printf("Average Value :: x:%.4f y:%.4f z:%.4f\n", average[0], average[1], average[2]);
+
+  calc_min(coord, min, number_points);
+  printf("Minimum Value :: x:%.4f y:%.4f z:%.4f \n", min[0], min[1], min[2]);
+
+  calc_max(coord, max, number_points);
+  printf("Maximum Value :: x:%.4f y:%.4f z:%.4f \n", max[0], max[1], max[2]);
+
+  calc_std(coord, average, std, number_points);
+  printf("Standard Deviation Value :: x:%.4f y:%.4f z:%.4f \n", std[0], std[1], std[2]);
+
+}
+/*******************************************************************************
+*
+* Objective: Computational time for each Task
+* Issues:
+*
+*******************************************************************************/
+void response_times() {
+  /* Print response_times */
+  printf("TASK 1: %LFs or %LFms\n", time2s(response_time[0]), time2ms(response_time[0]));
+  printf("TASK 2: %LFs or %LFms\n", time2s(response_time[1]), time2ms(response_time[1]));
+  printf("TASK 3: %LFs or %LFms\n", time2s(response_time[2]), time2ms(response_time[2]));
+
+  /* Clear responses times */
+  for (int i = 0; i < TASKS; i++) {
+    response_time[i] = SET(0, 0);
+  }
+}
 /*******************************************************************************
 *
 * Objective: Read file from directory and save data into a structure array
@@ -229,7 +346,6 @@ void calc_max(struct Coord *coord, float *max, int number_points) {
     }
 
 }
-
 /*******************************************************************************
 *
 * Objective: Calculate average points of a PointCloud file
@@ -301,7 +417,6 @@ void denoise(struct Coord *coord, float *std){
     }
     count_filtered_points=temp;
 }
-
 /*******************************************************************************
 *
 * Objective: Detect ramps
